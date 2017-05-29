aadd5710dfd1b539d857345bad08072124cae5fc
Stefan <stadolf@gmail.com>
2017-05-29 22:01:54 +0200

    step 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { AppRoutingModule }  from './app-routing.module';
 
-import { AppComponent }  from './app.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  providers: [ HeroService ],
+  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
